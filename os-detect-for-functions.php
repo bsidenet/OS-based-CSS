914237ef049755e008d10dfdce147851327c6732
Bsidenet <bsidenet@gmail.com>
2025-02-19 14:46:56 +0000

    Update os-detect-for-functions.php

--- os-detect-for-functions.php
+++ os-detect-for-functions.php
@@ -1,4 +1,4 @@
 function adicionar_script_deteccao() {
-    wp_enqueue_script('detectar-sistema-browser', get_template_directory_uri() . '/js/detectar.js', array(), null, true);
+	wp_enqueue_script('detectar-sistema-browser', get_template_directory_uri() . '/js/detectar.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'adicionar_script_deteccao');
